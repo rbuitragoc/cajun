eb9cc1ee52d77a833e4a15c691ed144a6b84b133
gguevara <gguevara@velocitypartners.net>
2015-01-29 15:25:31 -0500

    Changed configuration module.

--- config.js
+++ config.js
@@ -3,19 +3,43 @@ var SlackConnector = require("./classes/SlackConnector.class");
 var JSONConnector = require("./classes/persistence/JSONConnector.class");
 var MongoConnector = require("./classes/persistence/MongoConnector.class");
 
-// Sample Slack-thru-IRC config
+// Slack API config karmabot for tests
 module.exports = {
-	environment: "slashieMachine",
-	channel: "foundrystories",
-	server: "ffoundry.irc.slack.com",
-	botName: "obibot",
-	password: "ffoundry.MUr0IDVbPois5VgPtbYY",
-	connector: SlackConnector,
-	token: 'xoxb-3463721915-mXLDStOphmDcva9aBNBuYCcT',
+	environment: "platanoMachine",
+	channel: "karma",
+	botName: "karmabot",
+	token: 'xoxb-3534020363-hXwUqB5krA8fMZbxR7VmHsxB',
 	autoReconnect: true,
 	autoMark: true,
+	connector: SlackConnector,
 	persistence: JSONConnector
 };
+
+// Slack API config prod
+// module.exports = {
+// 	environment: "platanoMachine",
+// 	channel: "karma",
+// 	botName: "collabot",
+// 	token: 'xoxb-3558335570-MXw76pJPGbNLLyLi1KYWK8qr',
+// 	autoReconnect: true,
+// 	autoMark: true,
+// 	connector: SlackConnector,
+// 	persistence: JSONConnector
+// };
+
+// Sample Slack-thru-IRC config
+// module.exports = {
+// 	environment: "platanoMachine",
+// 	channel: "foundrystories",
+// 	server: "ffoundry.irc.slack.com",
+// 	botName: "obibot",
+// 	password: "ffoundry.MUr0IDVbPois5VgPtbYY",
+// 	connector: SlackConnector,
+// 	token: 'xoxb-3463721915-mXLDStOphmDcva9aBNBuYCcT',
+// 	autoReconnect: true,
+// 	autoMark: true,
+// 	persistence: JSONConnector
+// };
 
 // Sample plain IRC config
 
