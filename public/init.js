4470921b79b0207aeba018a5821f38b519b092f3
gguevara <gguevara@velocitypartners.net>
2015-02-11 13:06:23 -0500

    changed to call via https.

--- public/init.js
+++ public/init.js
@@ -1,7 +1,7 @@
 function startBot(){
 	$.ajax({
 		type: "POST",
-		url: 'http://salty-inlet-8617.herokuapp.com:3000',
+		url: 'https://salty-inlet-8617.herokuapp.com:3000',
 		data: '',
 		success: function( data, textStatus, jqXHR){
 			console.log(data);
