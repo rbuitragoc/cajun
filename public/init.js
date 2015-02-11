4c5d592875b3662489ec0cba2ad54007baf6a527
gguevara <gguevara@velocitypartners.net>
2015-02-11 13:08:08 -0500

    changed port.

--- public/init.js
+++ public/init.js
@@ -1,7 +1,7 @@
 function startBot(){
 	$.ajax({
 		type: "POST",
-		url: 'https://salty-inlet-8617.herokuapp.com:3000',
+		url: 'https://salty-inlet-8617.herokuapp.com',
 		data: '',
 		success: function( data, textStatus, jqXHR){
 			console.log(data);
