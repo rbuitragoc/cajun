30bb29726c427e4e4ae1b80c6e880cf27121f0d6
slashman <java.koder@gmail.com>
2015-02-13 17:06:50 -0500

    Add opening and closing brackets

--- classes/managers/ConversationManager.class.js
+++ classes/managers/ConversationManager.class.js
@@ -10,8 +10,9 @@ ConversationManager.prototype =  {
 		var manager = this;
 		this.bot.persistence.getConversation(topic, person, function(err, conversation){
 			if (conversation){
-				if (resumeCallback) 
+				if (resumeCallback){ 
 					resumeCallback(conversation);
+				}
 			} else {
 				var conversationData = {
 					topic: topic,
@@ -21,36 +22,42 @@ ConversationManager.prototype =  {
 					startedOn: new Date().formatYYYYMMDD()
 				}
 				manager.bot.persistence.insertConversation(conversationData, function(result, err){
-					if (!err)
+					if (!err){
 						callback();
+					}
 				})
 			}
 		});
 	},
 	endConversation: function(conversation, callback){
 		this.bot.persistence.deleteConversation(conversation.topic, conversation.withPlayer, function(result, err){
-			if (!err)
-				if (callback) callback();
-			else
+			if (!err) {
+				if (callback) {
+					callback();
+				}
+			} else {
 				console.log(err);
+			}
 		})
 	},
 	changeConversationState: function(conversation, newState, callback){
 		this.bot.persistence.updateConversation(conversation.topic, conversation.withPlayer, {$set: {state: newState}}, function(result, err){
-			if (!err)
+			if (!err) {
 				callback();
-			else
+			} else {
 				console.log(err);
+			}
 		})
 	},
 	setConversationData: function(conversation, key, value, callback){
 		var setModifier = { $set: {} };
 		setModifier.$set['data.' + key] = value;
 		this.bot.persistence.updateConversation(conversation.topic, conversation.withPlayer, setModifier, function(result, err){
-			if (!err)
+			if (!err) {
 				callback();
-			else
+			} else {
 				console.log(err);
+			}
 		})
 	}
 };
