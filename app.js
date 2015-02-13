973d3616b4a1e92aacd02250e58b8eac8166a377
gguevara <gguevara@velocitypartners.net>
2015-02-13 17:43:32 -0500

    Fixing start stop logic to avoid multiple instances.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Collabot = require("./classes/Collabot.class");
 var config = require("./config");
-var collabot = new Collabot(config);
+var collabot = null;
 
 var express = require('express');
 
@@ -16,16 +16,26 @@ app.get('/', function(req, res) {
 });
 
 app.post('/start', function (req, res) {
-	collabot = new Collabot(config);
-	collabot.start(function(status){
-		res.send(status);
-	});
+	if(collabot == null) {
+		console.log("Starting Collabot...");
+		collabot = new Collabot(config);		
+		collabot.start(function(status){
+			res.send(status);
+		});
+	} else {
+		res.send("already started");
+	}
 });
 app.post('/stop', function (req, res) {
-	collabot.stop(function(status){
-		res.send(status);
-	}); 
-  	
+	if(collabot){
+		console.log("Stopping Collabot...");
+		collabot.stop(function(status){
+			res.send(status);
+		});		
+		collabot = null;
+	} else {		
+		res.send("already stopped");
+	}
 });
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);
