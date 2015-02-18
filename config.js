e6c9889b3f8b3d1d3b7b764f083dd86c7f46fa61
Rick Buitrago <rbuitrago@velocitypartners.net>
2015-02-18 22:24:20 +0000

    config.js reverted to DEVELOPMENT (really we need to move this to env param)

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var MongoConnector = require("./classes/persistence/MongoConnector.class");
 // Slack API config karmabot for tests
 module.exports = {
 	version: "0.2.0",
-	environment: "PROD",
+	environment: "DEVELOPMENT",
 	channel: "gambit-test",
 	botName: "collabot",
 	token: 'xoxb-3558335570-MXw76pJPGbNLLyLi1KYWK8qr',
