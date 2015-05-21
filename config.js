ce8b07af8f8498afd7c6bf17b5d0c5bfcbbc5002
Rick Buitrago <rbuitrago@velocitypartners.net>
2015-05-21 12:24:35 -0500

    Done with release

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var MongoConnector = require("./classes/persistence/MongoConnector.class");
 
 // Slack API config karmabot for tests
 module.exports = {
-	version: "0.7.5",
+	version: "0.8.0",
 	environment: process.env.ENVIRONMENT,
 	// environment: "rick's lap",
 	channel: process.env.CHANNEL,
